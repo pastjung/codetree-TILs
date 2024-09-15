02900911022d2fef9b739bf240b943c11ebe1da8
정재현 <87860163+pastjung@users.noreply.github.com>
2024-09-15 20:32:55 +0900

    Title: 병합 정렬 구현, Time: 56ms, Memory: 1MB, Status: Passed - CodeTree

--- 240915/병합 정렬 구현/implement-merge-sort.cpp
+++ 240915/병합 정렬 구현/implement-merge-sort.cpp
@@ -6,28 +6,28 @@ int arr[SIZE] = {0, };
 int merged_arr[SIZE] = {0, };
 
 void merge(int start, int mid, int end){
-    int left = start, right = mid + 1, t = 1; 
+    int left = start, right = mid + 1, t = start; 
 
     // 병합
     while(left <= mid && right <= end){
-        if(arr[left] <= arr[right]) merged_arr[t++] = arr[left];
-        else merged_arr[t++] = arr[right];
+        if(arr[left] <= arr[right]) merged_arr[t++] = arr[left++];
+        else merged_arr[t++] = arr[right++];
     }
 
     // 왼쪽 배열 부분이 남은 경우
-    while(left <= mid) merged_arr[t++] = arr[left];
+    while(left <= mid) merged_arr[t++] = arr[left++];
 
     // 오른쪽 배열 부분이 남은 경우
-    while(right <= right) merged_arr[t++] = arr[right];
+    while(right <= end) merged_arr[t++] = arr[right++];
 
     // 배열 완성
-    for(int i = 0; i < end; i++){
+    for(int i = start; i <= end; i++){
         arr[i] = merged_arr[i];
     }
 }
 
 void merge_sort(int start, int end){
-    if(left < right){
+    if(start < end){
         int mid = (start + end) / 2;
         merge_sort(start, mid);
         merge_sort(mid + 1, end);
